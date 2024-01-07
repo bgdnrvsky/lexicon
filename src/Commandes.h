fcd92202672042976bd3c8a6606af08febe2ec7e
rosalieacco <rosalieacco@gmail.com>
2024-01-07 17:22:05 +0100

    ajout des param dictionnaire et is et changement de indents vers tab

--- src/Commandes.h
+++ src/Commandes.h
@@ -12,6 +12,7 @@ enum Status { COMMANDE_INVALIDE, MOT_INEXISTANT, SUCCES };
 /**
  * @brief Permet à un joueur piocher une carte du paquet et met une de ses
  * cartes sur le paquet de cartes exposees
+ * @param[in,out] is: input stream: la commande du joueur
  * @param[in,out] paquet: paquet de cartes a piocher
  * @param[in,out] joueur: joueur qui pioche
  * @param[in,out] exposees: paquet de cartes exposées
@@ -23,6 +24,7 @@ Status piocher(std::istringstream &is, Paquet &paquet, Joueur &joueur,
 
 /**
  * @brief Permet à un joueur de poser un mot sur la chaine de mots poses
+ * @param[in,out] is: input stream: la commande du joueur
  * @param[in,out] joueur: joueur qui pose le mot
  * @param[in,out] mots: chaine de mots poses
  * @param[in,out] dictionnaire: dictionnaire de mots valides
@@ -35,17 +37,21 @@ Status poser(std::istringstream &is, Joueur &joueur, Mots &mots,
 /**
  * @brief Remplace un mot pose par un nouveau mot construit a partir de celui-ci
  * en y inserant des lettres par d'autres lettre detenues par le joueur
+ * @param[in,out] is: input stream: la commande du joueur
  * @param[in,out] joueur: le joueur qui effectue le remplacement
  * @param[in,out] mots: la chaine de mots poses ou se trouve le mot a remplacer
- */
+  * @param[in,out] dictionnaire: dictionnaire de mots valides
+*/
 Status remplacer(std::istringstream &is, Joueur &joueur, Mots &mots,
                  const Dictionnaire &dictionnaire);
 
 /**
  * @brief Complète un mot pose par un nouveau mot construit a partir de celui-ci
  * en y inserant des lettres detenues par le joueur.
+ * @param[in,out] is: input stream: la commande du joueur
  * @param[in,out] joueur : le joueur qui complete le mot
  * @param[in,out] mots: la chaine de mots poses ou se trouve le mot a completer
+ * @param[in,out] dictionnaire: dictionnaire de mots valides
  */
 Status completer(std::istringstream &is, Joueur &joueur, Mots &mots,
                  const Dictionnaire &dictionnaire);
