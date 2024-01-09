81e09cfd7a197ec0df4e2e61bf7926e053a56540
iyksveiar <raevskijb@gmail.com>
2024-01-09 18:00:49 +0100

    Format + memory leak fix

--- test.cpp
+++ test.cpp
@@ -153,6 +153,7 @@ void test_poser() {
   ASSERT_EQ(poser(mot_joue_invalide, j, mots, d), COMMANDE_INVALIDE);
 
   detruire(j.main);
+  detruire(mots);
   EXIT
 }
 
@@ -178,88 +179,89 @@ void test_remplacer() {
   ajouter_carte(j1.main, e);
 
   ajouter_carte(j2.main, l);
-	ajouter_carte(j2.main,g);
+  ajouter_carte(j2.main, g);
 
   std::istringstream mot_orig("TE");
   poser(mot_orig, j1, mots, d);
 
   ASSERT_EQ(j1.main.restantes, 0);
 
-	//Cas 1: le mot peut etre remplace sans probleme
+  // Cas 1: le mot peut etre remplace sans probleme
   std::istringstream mot_valide("1 LE");
   ASSERT_EQ(remplacer(mot_valide, j2, mots, d), SUCCES);
 
-	//On verifie que le joueur a bien recupere la carte qu'il a remplace
-	ASSERT(carte_existe(j2.main,t));
+  // On verifie que le joueur a bien recupere la carte qu'il a remplace
+  ASSERT(carte_existe(j2.main, t));
   ASSERT_EQ(j2.main.restantes, 2);
 
-	//Cas 2: le mot ne peut pas etre remplace, le joueur n'a pas la lettre
+  // Cas 2: le mot ne peut pas etre remplace, le joueur n'a pas la lettre
   std::istringstream mot_invalide("1 ME");
   ASSERT_EQ(remplacer(mot_invalide, j2, mots, d), COMMANDE_INVALIDE);
 
-	//Cas 3: le mot de remplacement n'existe pas
-	std::istringstream mot_inexistant("1 GE");
-	ASSERT_EQ(remplacer(mot_inexistant, j2, mots, d), MOT_INEXISTANT);
+  // Cas 3: le mot de remplacement n'existe pas
+  std::istringstream mot_inexistant("1 GE");
+  ASSERT_EQ(remplacer(mot_inexistant, j2, mots, d), MOT_INEXISTANT);
 
-	//Cas 4: le mot de remplacement est plus long que le mot d'origine
-	std::istringstream mot_long("1 TES");
-	ASSERT_EQ(remplacer(mot_long, j2, mots, d), COMMANDE_INVALIDE);
+  // Cas 4: le mot de remplacement est plus long que le mot d'origine
+  std::istringstream mot_long("1 TES");
+  ASSERT_EQ(remplacer(mot_long, j2, mots, d), COMMANDE_INVALIDE);
 
   detruire(j1.main);
   detruire(j2.main);
-	detruire(mots);
+  detruire(mots);
   EXIT
 }
 
-void test_completer(){
-	INFO;
+void test_completer() {
+  INFO;
 
-	Joueur j1;
-	initialiser(j1, 1);
+  Joueur j1;
+  initialiser(j1, 1);
 
-	Joueur j2;
-	initialiser(j2, 2);
+  Joueur j2;
+  initialiser(j2, 2);
 
-	Dictionnaire dico;
-	initialiser(dico, "ods4.txt");
+  Dictionnaire dico;
+  initialiser(dico, "ods4.txt");
 
-	Mots mots;
-	initialiser(mots);
+  Mots mots;
+  initialiser(mots);
 
-	Carte r = nouvelle_carte('R'), o = nouvelle_carte('O'), i = nouvelle_carte('I'),
-				t = nouvelle_carte('T'), e = nouvelle_carte('E'), d = nouvelle_carte('D'),
-				s= nouvelle_carte('S'),w = nouvelle_carte('W');
+  Carte r = nouvelle_carte('R'), o = nouvelle_carte('O'),
+        i = nouvelle_carte('I'), t = nouvelle_carte('T'),
+        e = nouvelle_carte('E'), d = nouvelle_carte('D'),
+        s = nouvelle_carte('S'), w = nouvelle_carte('W');
 
-	ajouter_carte(j1.main,r);
-	ajouter_carte(j1.main,o);
-	ajouter_carte(j1.main,i);
-	ajouter_carte(j1.main,w);
+  ajouter_carte(j1.main, r);
+  ajouter_carte(j1.main, o);
+  ajouter_carte(j1.main, i);
+  ajouter_carte(j1.main, w);
 
-	ajouter_carte(j2.main,t);
-	ajouter_carte(j2.main,e);
-	ajouter_carte(j2.main,d);
-	ajouter_carte(j2.main,s);
+  ajouter_carte(j2.main, t);
+  ajouter_carte(j2.main, e);
+  ajouter_carte(j2.main, d);
+  ajouter_carte(j2.main, s);
 
-	std::istringstream mot_orig("ROI");
-	poser(mot_orig, j1, mots, dico);
+  std::istringstream mot_orig("ROI");
+  poser(mot_orig, j1, mots, dico);
 
-	//Cas 1: le mot peut etre complete sans probleme
-	std::istringstream mot_complete("1 DROITE");
-	ASSERT_EQ(completer(mot_complete,j2,mots,dico),SUCCES);
-	ASSERT_EQ(j2.main.restantes,1);
+  // Cas 1: le mot peut etre complete sans probleme
+  std::istringstream mot_complete("1 DROITE");
+  ASSERT_EQ(completer(mot_complete, j2, mots, dico), SUCCES);
+  ASSERT_EQ(j2.main.restantes, 1);
 
-	//Cas 2: le mot existe mais ne respecte pas l'ordre du mot d'origine
-	std::istringstream mot_invalide("1 SOIR");
-	ASSERT_EQ(completer(mot_invalide,j2,mots,dico),COMMANDE_INVALIDE);
+  // Cas 2: le mot existe mais ne respecte pas l'ordre du mot d'origine
+  std::istringstream mot_invalide("1 SOIR");
+  ASSERT_EQ(completer(mot_invalide, j2, mots, dico), COMMANDE_INVALIDE);
 
-	//Cas 3: le mot n'existe pas
-	std::istringstream mot_inexistant("1 SROI");
-	ASSERT_EQ(completer(mot_inexistant,j2,mots,dico),MOT_INEXISTANT);
+  // Cas 3: le mot n'existe pas
+  std::istringstream mot_inexistant("1 SROI");
+  ASSERT_EQ(completer(mot_inexistant, j2, mots, dico), MOT_INEXISTANT);
 
-	detruire(j1.main);
-	detruire(j2.main);
-	detruire(mots);
-	EXIT
+  detruire(j1.main);
+  detruire(j2.main);
+  detruire(mots);
+  EXIT
 }
 
 int main(void) {
@@ -271,5 +273,5 @@ int main(void) {
   test_joueur();
   test_poser();
   test_remplacer();
-	test_completer();
+  test_completer();
 }
